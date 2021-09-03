cff3fff4ae72ed9ab9a80fd886724b00fc4dc546
jorge perez <jorgeperez00@hotmail.com>
2021-09-03 17:01:18 -0400

    adding multi dim arry tutorial

---
+++ tutorials/arrays/arrays.js
@@ -0,0 +1,15 @@
+function multiDimensionArray() {
+    // array creation
+    let ticTacToeBoard = [
+        ['x', '12', ''],
+        ['', '0', ''],
+        ['', '', '']
+    ];
+    // getting values
+    let value = ticTacToeBoard[0][0];
+    console.log('value is ' + value);
+    value = ticTacToeBoard[1][1];
+    console.log('value is ' + value);
+    value = ticTacToeBoard[0][2];
+    console.log('value is ' + value);
+}
