3c750e4a6568cdb47c4f1766dd9e5dedb4433046
jorge perez <jorgeperez00@hotmail.com>
2021-09-12 16:45:27 -0400

    fixed validation bug

--- tic-tac-toe/components/game/game.js
+++ tic-tac-toe/components/game/game.js
@@ -5,52 +5,59 @@ let row;
 let col;
 
 function Initialize() {
-    boardState.push(["", "", ""],["", "", ""],["", "", ""]);
+    boardState.push(["", "", ""], ["", "", ""], ["", "", ""]);
 }
 
+/**
+ * Check to see if player played in a valid cell.  It also check if someone won.
+ * @param {*} element 
+ * @returns 
+ */
 function ValidatePlay(element) {
     // get location
     cellLocation = element.getAttribute('name');
-    row = parseInt(cellLocation.substr(0,1));
-    col = parseInt(cellLocation.substr(1,1));
+    row = parseInt(cellLocation.substr(0, 1));
+    col = parseInt(cellLocation.substr(1, 1));
     // validate if cell is taken.
     let cellValue = boardState[row][col];
     if (cellValue != "") {
         alert("cell taken");
-        return;
+        return false;
     }
     // validate if game over
-
+    return true;
 }
 
 function DrawBoard() {
     for (let row = 0; row < 3; row++) {
         for (let col = 0; col < 3; col++) {
-            let cellElement = document.querySelector('[name="'+ col.toString() + row.toString() + '"]');            
+            let cellElement = document.querySelector('[name="' + col.toString() + row.toString() + '"]');
             cellElement.textContent = boardState[col][row];
             console.log(`col: ${col} row: ${row} value: ${cellElement.textContent} el name: ${cellElement.getAttribute('name')}`)
-        }        
+        }
     }
 }
 
 /** 
  * saves the user play on board state
- **/  
+ **/
 function SavePlayerMove() {
-    boardState[row][col] = playerOneTurn == true ? 'x' : 'o' ;
+    boardState[row][col] = playerOneTurn == true ? 'x' : 'o';
 }
 
 function clickHandler(element) {
-    ValidatePlay(element)
-    SavePlayerMove();
-    DrawBoard();
-    ShowPlayerNumber();
-    playerOneTurn = !playerOneTurn;
+    if (ValidatePlay(element) == true) {
+        // validate passed
+        SavePlayerMove();
+        DrawBoard();
+        ShowPlayerNumber();
+        playerOneTurn = !playerOneTurn;
+    }
 }
 
 function ShowPlayerNumber() {
     let playerTurnElement = document.querySelector('.player-number');
-    let playerNumber = playerOneTurn == true ? 1 : 2 ;
+    let playerNumber = playerOneTurn == true ? 1 : 2;
     playerTurnElement.textContent = playerNumber;
 }
 
